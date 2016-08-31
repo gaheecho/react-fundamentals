7a3d0427c07a5c930fbb3f07f3201c303076cac7
gaheeCho <gahee.cho@traport.com>
2016-08-31 19:07:14 +0900

    생명주기 테스트 추가

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,6 +25,59 @@ export default class Contact extends React.Component {
         this.handleChange = this.handleChange.bind(this)
     }
 
+    /*
+    컴포넌트 생성할 때 constructor -> componentWillMount -> render -> componentDidMount
+    컴포넌트의 props가 변경될 때  componentWillReceiveProps -> shouldComponentUpdate -> componentWillUpdate-> render -> componentDidUpdate
+    state가 변경될 때 shouldComponentUpdate -> componentWillUpdate-> render -> componentDidUpdate
+    */
+
+    /*
+    최초 렌더링이 일어나기 직전
+    */
+    componentWillMount () {
+
+    };
+
+    /*
+    최초 렌더링이 일어난 다음 호출
+    */
+    componentDidMount() {
+
+    };
+    /*
+    업데이트 시
+    */
+    componentWillReceiveProps (nextProps) {
+
+    };
+    /*
+    업데이트 시
+    */
+    shouldComponentUpdate(nextProps, nextState) {
+      console.log('this.state', this.state);
+      console.log('this.state.keyword', this.state.keyword);
+      console.log('nextState', nextState);
+      console.log('nextState.keyword', nextState.keyword);
+    return this.state !== nextState;
+    };
+    /*
+    업데이트 시
+    */
+    componentWillUpdate (nextProps, nextState) {
+
+    };
+    /*
+    업데이트 시
+    */
+    componentDidUpdate (prevProps, prevState) {
+
+    };
+    /*
+    DOM에서 마운트 해제되기 직전
+    컴포넌트를 제거할 때는 componentWillUnmount만 실행됨.
+    */
+    componentWillUnmount () {
+    };
     handleChange(e){
         this.setState({
             keyword: e.target.value
@@ -42,7 +95,7 @@ export default class Contact extends React.Component {
                 );
             return data.map((contact, i) => {
                 console.log(contact, i);
-                return (<ContactInfo contact={contact} key={i}/>);
+                return (<ContactInfo contact={contact} keyword = {this.state.keyword} key={i}/>);
             });
         };
 
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,9 +1,60 @@
 import React from 'react';
 
 export default class ContactInfo extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  /*
+  최초 렌더링이 일어나기 직전
+  */
+  componentWillMount () {
+
+  };
+
+  /*
+  최초 렌더링이 일어난 다음 호출
+  */
+  componentDidMount() {
+    console.log('this.props in ComtiactInfo', this.props);
+  };
+  /*
+  업데이트 시
+  */
+  componentWillReceiveProps (nextProps) {
+
+  };
+  /*
+  업데이트 시
+  */
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('this.props in contactInfo', this.props);
+    console.log('nextProps in contactInfo', nextProps);
+  return this.props !== nextProps;
+  };
+  /*
+  업데이트 시
+  */
+  componentWillUpdate (nextProps, nextState) {
+
+  };
+  /*
+  업데이트 시
+  */
+  componentDidUpdate (prevProps, prevState) {
+
+  };
+  /*
+  DOM에서 마운트 해제되기 직전
+  */
+  componentWillUnmount () {
+  };
     render() {
         return (
-            <div>{this.props.contact.name} {this.props.contact.phone}</div>
+            <div>{this.props.contact.name} {this.props.contact.phone} {this.props.keyword}</div>
         );
     }
 }
